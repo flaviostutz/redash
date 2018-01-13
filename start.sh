9873f89cd5cd4b11edf01fce99726930a98212e8
flaviostutz <flaviostutz@gmail.com>
2018-01-13 16:58:23 -0200

    if on inti

--- start.sh
+++ start.sh
@@ -2,12 +2,17 @@
 
 echo "STARTING REDASH - $1"
 
-echo "Executing /app/bin/docker-entrypoint server create_db..."
-cd /app
-until /app/bin/docker-entrypoint create_db
-do
-    echo "Retrying..."
-done
+if [ ! -f initdone ]; then
+    echo "Executing /app/bin/docker-entrypoint server create_db..."
+    cd /app
+    until /app/bin/docker-entrypoint create_db
+    do
+        sleep 5
+        echo "Retrying..."
+    done
+    touch initdone
+fi
 
 echo "Running $1..."
 /app/bin/docker-entrypoint $1
+
